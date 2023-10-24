b7723d35c644493862a6ae26423e7ca55e309a92
Sarah Rehan <sarahahmad17@gmail.com>
2023-10-23 22:22:43 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders h1 heading 25 + 5 Clock', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  let wrapper = container.querySelector('h1');
+  expect(wrapper?.textContent).toEqual('25 + 5 Clock');
 });
